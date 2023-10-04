8ec623036b6a8e45efd241c531bfcadc46bd4b43
Skai <tyronekisienya01@gmail.com>
2023-10-04 21:20:30 +0300

    prints last digit of value passed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lst_digit = abs(number) % 10
-if number < 0:
-    lst_digit = -lst_digit
-    print(f"Last digit of {number} is {lst_digit} and is", end = ' ')
-    if lst_digit > 5:
-        print("greater than 5")
-    elif lst_digit == 0:
-        print("0")
-    else:
-        print("less than 6 and not 0")
+lst = abs(number) % 10
+if number< 0:
+    lst = -lst
+print(f"Last digit of {number} is {lst} and is", end=" ")
+if lst > 5:
+    print(f"greater than 5")
+elif lst == 0:
+    print(f"0")
+else:
+    print(f"less than 6 and not 0")
