7b1f116c629b128c321076d8e1563eed75a9baeb
Skai <tyronekisienya01@gmail.com>
2023-10-08 17:00:02 +0300

    prints last digit of value passed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 lst = abs(number) % 10
-if number< 0:
+if number < 0:
     lst = -lst
 print(f"Last digit of {number} is {lst} and is", end=" ")
 if lst > 5:
