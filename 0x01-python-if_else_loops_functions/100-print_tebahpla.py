9211b417b638ac88281b8f50c9ba40a4be286b32
skai <tyronekisienya01@gmail.com>
2023-10-09 12:26:11 +0300

    reverse print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for aphla in range(122, 96, -1):
-        if aphla % 2 == 0:
-                print("{}".format(chr(aphla)), end="")
-        else:
-                print("{}".format(chr((aphla) - 32)) ,end="")
+    if aphla % 2 == 0:
+        print("{}".format(chr(aphla)), end="")
+    else:
+        print("{}".format(chr((aphla) - 32)), end="")
