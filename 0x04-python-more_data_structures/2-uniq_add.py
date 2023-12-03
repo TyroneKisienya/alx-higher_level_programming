1ccf7180e8cb0da039cecb8a50d8f88ffbd70630
Skai <135860214+TyroneKisienya@users.noreply.github.com>
2023-12-03 05:46:13 +0300

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/2-uniq_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def uniq_add(my_list=[]):
-    add = 0
-    for items in set(my_list):
-        add += items
-    return add
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-
-def print_sorted_dictionary(a_dictionary):
-    list(print(f"{key}: {a_dictionary[key]}") for key in sorted(a_dictionary))
